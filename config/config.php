841b5b7510d94b2a94bd158571b477f0c3f8c21f
Jules <jules.claussen@sfr.fr>
2018-03-14 14:31:47 +0100

    Prise en compte serveur

--- config/config.php
+++ config/config.php
@@ -18,24 +18,24 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // DB
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	define('_MYSQL_HOST','localhost');
-	define('_MYSQL_DBNAME','projetweb');
-	define('_MYSQL_USER','root');
-	define('_MYSQL_PASSWORD','root');
+	// define('_MYSQL_HOST','localhost');
+	// define('_MYSQL_DBNAME','projetweb');
+	// define('_MYSQL_USER','root');
+	// define('_MYSQL_PASSWORD','root');
 	
 
-	// if($_SERVER['DOCUMENT_ROOT']=='/Users/luc/Sites') {
-		// define('_MYSQL_HOST','localhost');
-		// define('_MYSQL_PORT',8889);	
-		// define('_MYSQL_DBNAME','MinesRails');
-		// define('_MYSQL_USER','root');
-		// define('_MYSQL_PASSWORD','root');
-	// } else {
-		// on the isic server for example
-		// define('_MYSQL_HOST','localhost');
-		// define('_MYSQL_DBNAME','luc');
-		// define('_MYSQL_USER','root');
-		// define('_MYSQL_PASSWORD','root');		
-	// }
+	if($_SERVER['DOCUMENT_ROOT']=='D:\Mes Documents\Logiciels\Uwamp\www\ProjetWeb') {
+		define('_MYSQL_HOST','localhost');
+		define('_MYSQL_PORT',8889);	
+		define('_MYSQL_DBNAME','projetweb');
+		define('_MYSQL_USER','root');
+		define('_MYSQL_PASSWORD','root');
+	} else {
+		//on the isic server for example
+		define('_MYSQL_HOST','localhost');
+		define('_MYSQL_DBNAME','jules_claussen');
+		define('_MYSQL_USER','jules.claussen');
+		define('_MYSQL_PASSWORD','RhKHudMI');		
+	}
 	
 ?>
