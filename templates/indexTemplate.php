6bb9a6ca5049132ff530bf1f1336d297b5217130
Jules <jules.claussen@sfr.fr>
2018-03-28 09:18:26 +0200

    Ajoute dans Model

--- templates/indexTemplate.php
+++ templates/indexTemplate.php
@@ -4,7 +4,7 @@
 
 
   <head>
-
+	 <!-- Bootstrap core CSS -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
