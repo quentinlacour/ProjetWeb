2029b0efc4382f30709733f6e7b03f3ab2090bd0
Quentin <lacour.quentin76@gmail.com>
2018-04-04 17:26:21 +0200

    New Form

--- templates/User/indexTemplate.php
+++ templates/User/indexTemplate.php
@@ -1,39 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-    <!-- Intro Header -->
-    <header class="masthead">
+  <div class="masthead">
     <div class="container">
       <div class="intro-text">
         <div class="intro-lead-in">Bienvenue sur Travel Mines !</div>
-        <div class="intro-heading text-uppercase">Pas envie de marcher ?</div>
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Trouver une Voiture</a>
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Proposer un trajet</a>
+          <div class="intro-heading text-uppercase">Pas envie de marcher ?</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Trouver une Voiture</a>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Proposer un trajet</a>
+        </div>
       </div>
-    </div>
-  </header>
+</div>
 
-	
-
-  
-
-    <!-- Map Section -->
-    <div id="map"></div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/grayscale.min.js"></script>
-  	<link rel="stylesheet" href="css/style.css">
-  </body>
-
-</html>
