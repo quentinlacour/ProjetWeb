915d2f69f05c2d3fcc4b13a9261b00f630641c0d
Quentin <lacour.quentin76@gmail.com>
2018-04-11 00:37:21 +0200

    css insc

--- templates/Anonymous/inscriptionTemplate.php
+++ templates/Anonymous/inscriptionTemplate.php
@@ -11,31 +11,32 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required data-validation-required-message="Please enter your name.">
+                    <input class="form-control" id="inputLogin" name="inscLogin" placeholder="Login" required data-validation-required-message="Please enter your name.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required data-validation-required-message="Please enter your email address.">
+                    <input class="form-control" id="inputPassword" name="inscPassword" type="password" placeholder="Mot de passe" required data-validation-required-message="Please enter your password.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required data-validation-required-message="Please enter your phone number.">
+                    <input class="form-control" id="inputTelephone" name="inputTelephone" placeholder="Telephone" required data-validation-required-message="Please enter your phone number.">
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
+                <div class="form-group">
+                    <input class="form-control" id="inputFirstname" name="inputFirstname" placeholder="Prenom" required data-validation-required-message="Please enter your phone number.">
+                    <p class="help-block text-danger"></p>
+                  </div>
                    <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required data-validation-required-message="Please enter your name.">
+                    <input class="form-control"id="inputLastname" name="inputLastname" placeholder="Nom" required data-validation-required-message="Please enter your name.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required data-validation-required-message="Please enter your email address.">
+                    <input class="form-control" id="inputEmail" name="inputEmail" placeholder="Email" required data-validation-required-message="Please enter your email address." type="email">
                     <p class="help-block text-danger"></p>
                   </div>
-                  <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required data-validation-required-message="Please enter your phone number.">
-                    <p class="help-block text-danger"></p>
-                  </div>
+                  
                 </div>
                 <div class="clearfix"></div>
                 <div class="col-lg-12 text-center">
@@ -47,71 +48,8 @@
           </div>
         </div>
       </div>
-    </section>
+</section>
 
 
 
-
-<div class="container auth">
-    <div id="big-form" class="well auth-box">
-      <form action="index.php?a=validateInscription" method="post">
-        <fieldset>
-          <!-- Text input-->
-          <div class="form-group">
-            <div class="form-group">
-              <input id="inputLogin" name="inscLogin" placeholder="Login" class="form-control input-md" type="text">
-            </div>
-          </div>
-
-          <!-- Password input-->
-          <div class="form-group">
-            <div class="">
-              <input id="inputPassword" name="inscPassword" placeholder="Mot de passe" class="form-control input-md" type="password">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="">
-              <input id="inputConfirmPassword" name="inscPassword2" placeholder="Confirmer mot de passe" class="form-control input-md" type="password">
-            </div>
-          </div>
-          <!-- Name input-->
-          <div class="form-group">
-            <div class="form-group">
-              <input id="inputLastname" name="inputLastname" placeholder="Nom" class="form-control input-md" type="text">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="form-group">
-              <input id="inputFirstname" name="inputFirstname" placeholder="Prenom" class="form-control input-md" type="text">
-            </div>
-          </div>
-
-           <!-- Phone input-->
-           <div class="form-group">
-            <div class="form-group">
-              <input id="inputTelephone" name="inputTelephone" placeholder="Telephone" class="form-control input-md" type="text">
-            </div>
-          </div>
-
-          <!-- Mail input-->
-          <div class="form-group">
-            <div class="form-group">
-              <input id="inputEmail" name="inputEmail" placeholder="Email" class="form-control input-md" type="email">
-            </div>
-          </div>
-          
-
-          <!-- Création compte-->
-          <div class="btn-group" >
-            <input type="submit" value="Créer mon compte" name="boutonCreerCompte" class="btn btn-default" ></input>
-          </div>
-
-        </fieldset>
-      </form>
-    </div>
-    <div class="clearfix"></div>
-  </div>
-  
   
