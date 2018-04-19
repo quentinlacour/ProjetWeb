fd00c9de664b8c7b944a6a22a4e976dacb7dac36
Quentin <lacour.quentin76@gmail.com>
2018-04-19 20:27:50 +0200

    color2

--- templates/Trajet/statistiquesTemplate.php
+++ templates/Trajet/statistiquesTemplate.php
@@ -78,9 +78,8 @@
 <style type="text/css">
 .popupStat{
   position: absolute;
-  left: 42%;
-  top: 50%;
-  
+  margin: auto;
+  margin-top: -200px;
   z-index: 999;
   width: 300px;
   background: #fed136;
@@ -94,6 +93,7 @@
   opacity: 0.85;
   display: block;
   visibility: hidden;
+
 }
 </style>
 
@@ -144,7 +144,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:nbTrajetParJour('hide');">Close</b></center> 
+		<center><a href="javascript:nbTrajetParJour('hide');">Close</a></center> 
 	</div>
 							<!-- Trajets par mois --> 
 	<div class="popupStat" id="trajetMois" name="boutonTrajetParMois"> 
@@ -164,7 +164,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:nbTrajetParMois('hide');">Close</b></center> 
+		<center><a href="javascript:nbTrajetParMois('hide');">Close</a></center> 
 	</div>
 							<!-- Trajets par an --> 
 	<div class="popupStat" id="trajetAn" name="boutonTrajetParAn"> 
@@ -184,7 +184,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:nbTrajetParAnnee('hide');">Close</b></center> 
+		<center><a href="javascript:nbTrajetParAnnee('hide');">Close</a></center> 
 	</div>
 							<!-- top10PersonnesCreatrices --> 
 	<div class="popupStat" id="top10PersonnesCreatrices" name="boutonTrajetParJour"> 
@@ -207,7 +207,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:top10PersonnesCreatrices('hide');">Close</b></center> 
+		<center><a href="javascript:top10PersonnesCreatrices('hide');">Close</a></center> 
 	</div>
 
 							<!-- top10PersonnesParticipatrices --> 
@@ -228,7 +228,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:top10PersonnesParticipatrices('hide');">Close</b></center> 
+		<center><a href="javascript:top10PersonnesParticipatrices('hide');">Close</a></center> 
 	</div>	
 
 							<!-- Ville De Départ --> 
@@ -249,7 +249,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:top10VilleDepart('hide');">Close</b></center> 
+		<center><a href="javascript:top10VilleDepart('hide');">Close</a></center> 
 	</div>	
 	
 							<!-- Ville De Arrivee --> 
@@ -270,7 +270,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:top10VilleDestination('hide');">Close</b></center> 
+		<center><a href="javascript:top10VilleDestination('hide');">Close</a></center> 
 	</div>	
 	
 							<!-- Ville D'Arrivee --> 
@@ -288,7 +288,7 @@
 		}?>
 		</ul> 
 		<br />
-		<center><b href="javascript:nombreMoyenPersonneParTrajet('hide');">Close</b></center> 
+		<center><a href="javascript:nombreMoyenPersonneParTrajet('hide');">Close</a></center> 
 	</div>
 	
 	  
